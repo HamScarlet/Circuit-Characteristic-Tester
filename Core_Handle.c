30adc8cdea860658a43db7c0d8b0d22acfd7a633
zhujin <zhujin2024@163.com>
2026-04-30 20:39:07 +0800

    初始化

---
+++ Core_Handle.c
@@ -0,0 +1,14 @@
+#include "stm32h7xx.h"                  // Device header
+
+
+void Core_Init(void)
+{
+	
+}
+
+
+void Core_Loop(void)
+{
+	
+}
+
---
+++ Core_Handle.h
@@ -0,0 +1,9 @@
+#ifndef __CORE_HANDLE_H
+#define __CORE_HANDLE_H
+
+
+void Core_Init(void);
+void Core_Loop(void);
+
+
+#endif
